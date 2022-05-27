34b0cfc1512a13605407e58ebf74089be390f61a
fstipo <fstipanov@gmail.com>
2022-05-27 09:25:44 +0200

    add Header names

--- src/components/Pages/Dashboard/Grid.js
+++ src/components/Pages/Dashboard/Grid.js
@@ -1,15 +1,15 @@
-import React, { useState, useRef } from 'react';
+import React, { useState } from 'react';
 import { AgGridReact } from 'ag-grid-react';
 import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
 import useAxios from '../../../hooks/useAxios';
-import { userData } from './Data';
-import { Link } from 'react-router-dom';
+import { userData } from './data';
+import { gridColumns } from './gridColumns';
 import Modal from '../../UI/Modal';
 
 const Grid = () => {
-  const gridRef = useRef(null);
-  const [showModal, setShowModal] = useState(false);
+  // const gridRef = useRef(null);
+  // const [showModal, setShowModal] = useState(false);
   const {
     // isLoading,
     // isError,
@@ -19,87 +19,16 @@ const Grid = () => {
   let notesEl = [...notes, ...userData];
 
   const onDetailsClick = () => {};
-  const details = (p) => (
-    <button className="btn btn-link" onClick={onDetailsClick}>
-      <Link to="/details">...details</Link>
-    </button>
-  );
 
   const gridOptions = {
     pagination: true,
     paginationAutoPageSize: true,
   };
 
-  const [columnDefs] = useState([
-    {
-      field: 'id',
-      checkboxSelection: true,
-      sortable: 'true',
-      filter: 'true',
-      headerTooltip: 'User id',
-      resizable: true,
-      width: 90,
-    },
-    {
-      field: 'name',
-      sortable: 'true',
-      filter: 'true',
-      headerTooltip: 'Name',
-    },
-    {
-      field: 'username',
-      sortable: 'true',
-      filter: 'true',
-      headerTooltip: 'Username',
-    },
-    {
-      field: 'email',
-      sortable: 'true',
-      filter: 'true',
-      headerTooltip: 'Email',
-    },
-    {
-      field: 'phone',
-      sortable: 'true',
-      filter: 'true',
-      headerTooltip: 'Phone',
-    },
-    {
-      field: 'website',
-      sortable: 'true',
-      filter: 'true',
-      headerTooltip: 'Website',
-    },
-    {
-      field: 'address.city',
-      sortable: 'true',
-      filter: 'true',
-      headerTooltip: 'City',
-    },
-    {
-      field: 'address.street',
-      sortable: 'true',
-      filter: 'true',
-      headerTooltip: 'Street',
-    },
-    {
-      field: 'address.zipcode',
-      sortable: 'true',
-      filter: 'true',
-      headerTooltip: 'City zipCode',
-    },
-    {
-      field: 'company.name',
-      sortable: 'true',
-      filter: 'true',
-      headerTooltip: 'Company',
-    },
-    { field: 'details', headerTooltip: 'Details', cellRenderer: details },
-  ]);
+  const [columnDefs] = useState(gridColumns);
 
   const modalHandler = () => {
-    setShowModal(true);
-
+    // setShowModal(true);
     // if (typeof data !== 'undefined') {
     //   setUserData(data);
     //   setUserData((state) => {
---
+++ src/components/Pages/Dashboard/gridColumns.js
@@ -0,0 +1,77 @@
+import { Link } from 'react-router-dom';
+const details = (p) => (
+  <button className="btn btn-link">
+    <Link to="/details">...details</Link>
+  </button>
+);
+
+export const gridColumns = [
+  {
+    field: 'id',
+    checkboxSelection: true,
+    sortable: 'true',
+    filter: 'true',
+    headerTooltip: 'User id',
+    resizable: true,
+    width: 90,
+  },
+  {
+    headerName: 'Full Name',
+    field: 'name',
+    sortable: 'true',
+    filter: 'true',
+    headerTooltip: 'Name',
+  },
+  {
+    field: 'username',
+    sortable: 'true',
+    filter: 'true',
+    headerTooltip: 'Username',
+  },
+  {
+    field: 'email',
+    sortable: 'true',
+    filter: 'true',
+    headerTooltip: 'Email',
+  },
+  {
+    field: 'phone',
+    sortable: 'true',
+    filter: 'true',
+    headerTooltip: 'Phone',
+  },
+  {
+    field: 'website',
+    sortable: 'true',
+    filter: 'true',
+    headerTooltip: 'Website',
+  },
+  {
+    headerName: 'City',
+    field: 'address.city',
+    sortable: 'true',
+    filter: 'true',
+    headerTooltip: 'City',
+  },
+  {
+    headerName: 'Street',
+    field: 'address.street',
+    sortable: 'true',
+    filter: 'true',
+    headerTooltip: 'Street',
+  },
+  {
+    headerName: 'Zip code',
+    field: 'address.zipcode',
+    sortable: 'true',
+    filter: 'true',
+    headerTooltip: 'City zipCode',
+  },
+  {
+    field: 'company.name',
+    sortable: 'true',
+    filter: 'true',
+    headerTooltip: 'Company',
+  },
+  { field: 'details', headerTooltip: 'Details', cellRenderer: details },
+];
