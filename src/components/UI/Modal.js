cebe42da322b551586fffe3c360334063142a569
fstipo <fstipanov@gmail.com>
2022-05-30 17:11:51 +0200

    Modal

--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -4,22 +4,43 @@ function ModalShow(props) {
   return (
     <Modal
       {...props}
-      size="lg"
+      size="md"
       aria-labelledby="contained-modal-title-vcenter"
       centered
     >
-      <Modal.Header closeButton>
-        <Modal.Title id="contained-modal-title-vcenter">
+      <Modal.Header className="bg-primary text-white" closeButton>
+        <Modal.Title className="display-6 " id="contained-modal-title-vcenter">
           {props.data.name}
         </Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        <h4>Centered Modal</h4>
-        <p>
-          Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
-          dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
-          consectetur ac, vestibulum at eros.
-        </p>
+        <ul className="d-flex flex-column gap-2">
+          <li className="fs-5">
+            <strong>Full Name:&nbsp;</strong>
+            {props.data.name}
+          </li>
+          <li className="fs-5">
+            <strong>Username:&nbsp; </strong> {props.data.username}
+          </li>
+          <li className="fs-5">
+            <strong>Email:&nbsp;</strong> {props.data.email}
+          </li>
+
+          <li className="fs-5">
+            <strong>Phone:&nbsp;</strong> {props.data.phone}
+          </li>
+          <li className="fs-5">
+            <strong>Website:&nbsp;</strong> {props.data.website}
+          </li>
+          {/* <li className="fs-5">
+            <strong>City:&nbsp;</strong>
+            {props.data.address.city}
+          </li>
+          <li className="fs-5">
+            <strong>Street:&nbsp;</strong>
+            {props.data.address.street}
+          </li> */}
+        </ul>
       </Modal.Body>
       <Modal.Footer>
         <Button onClick={props.onHide}>Close</Button>
